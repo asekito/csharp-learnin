896bae7da206d278dc25aae3cb70ccd9ca030a20
asekito <email@email.com>
2020-10-11 20:21:42 -0700

    tutorial up to constants

---
+++ DataType.cs
@@ -0,0 +1,10 @@
+// using System;
+
+// namespace DataTypeApplication {
+//   class DataType {
+//     static void Main(string[] args) {
+//       Console.WriteLine("Size of int: {0}", sizeof(int));
+//       Console.ReadLine();
+//     }
+//   }
+// }
