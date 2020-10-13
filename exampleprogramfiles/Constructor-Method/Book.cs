8da8777cd1ff6e174651fca29782d4dcf8af99d3
asekito <email@email.com>
2020-10-13 16:29:49 -0700

    constructor

---
+++ exampleprogramfiles/Constructor-Method/Book.cs
@@ -0,0 +1,16 @@
+// using System;
+
+// namespace csharp_learnin {
+//   class Book {
+//     public string title;
+//     public string author;
+//     public int pages;
+
+//     public Book(string aTitle, string aAuthor, int aPages) { // a constructor method <---- constructor for class will get called to when creating object of this class, special method in class that will get called
+//         title = aTitle;
+//         author = aAuthor;
+//         pages = aPages;
+//     }
+
+//   }
+// }
---
+++ exampleprogramfiles/Constructor-Method/Program.cs
@@ -0,0 +1,17 @@
+// using System;
+
+// namespace csharp_learnin
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Book book1 = new Book("Song of Achilles", "IDK", 400);
+
+//             Book book2 = new Book("Lord of the flies", "Don't know", 100);
+
+//             Console.WriteLine(book1.title);
+//             Console.ReadLine();
+//         }
+//     }
+// }
